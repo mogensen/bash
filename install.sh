16c1b051a912d661479dfd933c05c77c2087bf4e
Frederik Mogensen <sodiaf@gmail.com>
2011-03-24 13:22:08 +0100

    Created a install script, and renamed some files to work with it

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function printStat {
+    if [ $? == 0 ]; then
+		echo "[${GREEN}   OK   ${ATTR_RESET}]" $1
+    else
+		echo "[${RED} FAILED ${ATTR_RESET}]" $1
+    fi
+}
+
+for i in _*
+do 
+	source="${PWD}/$i"
+	target="${HOME}/${i/_/.}"
+	ln -sf ${source} ${target}
+	printStat "ln -sf ${source} ${target}"
+done
